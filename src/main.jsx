869084d87b83704b27f1d4505dfe665f5dd50337
MdShariarAbrar <shariara@outlook.com>
2024-03-31 23:49:15 +0600

    Root Home Footer component created and setup router

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Root from "./components/Root/Root.jsx";
+import Home from "./components/Root/Home/Home.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
